219055b441d4499b00812e94f5163d5c1280ceac
Sam Sheffres <ssheffres@gmail.com>
2016-09-07 14:29:17 +0600

    Add Temporary Object + Script, Make Pedestals Longer, Update Pedestal Manager for new Pedestal Height

---
+++ Assets/Scripts/TemporaryObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemporaryObject : MonoBehaviour {
+
+	public float SelfDestructDelay = 1.0f;
+	public bool DestroyImmdediatly = true;
+
+	void Awake () {
+		if (DestroyImmdediatly) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Start () {
+		StartCoroutine ("DestroyAfterDelay");
+	}
+
+	internal IEnumerator DestroyAfterDelay() {
+		yield return new WaitForSeconds(SelfDestructDelay);
+		Destroy (gameObject);
+	}
+}
